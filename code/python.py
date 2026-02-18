59d9713ba7d66efa86abfc3054ddb4d3dc4fb9f5
codeby-umar <codingbyumar@gmail.com>
2026-02-18 18:11:33 +0500

    homework

---
+++ code/python.py
@@ -0,0 +1,56 @@
+# ism = "Abdulbosid"
+# value = ism[5:-3]
+# print(value)
+
+# python = "python is very popular"
+# print(python)
+# code = python[::-1]
+# print(code)
+
+
+# length = str("python juda qiyin tili xisoblandi")
+# code = length.count("i")
+# print(code)
+
+
+# username = input("username kiriting :")
+# cut = username.replace(" " , "_")
+# print(cut)
+
+
+
+# text = "Javascript is "
+# fake_You = text.find("is")
+# print(fake_You)
+
+
+# texts = input("Sizning ismingiz va familyanigz :")
+# hook = texts.title()
+# print(hook)
+
+
+
+"""Instagram webiste login va parol tizimi"""
+
+login = str(input("Login kiriting :"))
+password = str(input("Password kitiing :"))
+
+log = "shukurullo45@gmail.com"
+password1 = "code6969"
+
+full_Code = log.replace(" " , "_").lower()
+
+if  login == log:
+    print("------------------------------")
+    print("Sizning logininngiz to'gri ")
+    print("------------------------------")
+elif password1 == password:
+    print("------------------------------")
+    print("Sizning logininngiz to'gri ")
+    print("------------------------------")
+else:
+    print("------------------------------")
+    print("Sizda xatolik bor Error")
+    print("------------------------------")
+
+

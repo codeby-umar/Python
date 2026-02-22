499434be61e681c02eedf9cca36f3ff4871f6206
codeby-umar <codingbyumar@gmail.com>
2026-02-22 23:14:25 +0500

    Binary learning

--- code/number.py
+++ code/number.py
@@ -1,6 +1,11 @@
-# arifmatik operator
-a = 20
-b = 3
-c = a % b
-print(c)
+# https://docs.python.org/3/library/math.html - shundan korib chiqamiz
+
+
+# Binaryga misollar 
+
+harf = input("Harf kiriting: ")
+dala = format(ord(harf[0]) , 'b')
+print(dala)
+
+
 
